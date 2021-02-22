85c44b608800e98db2cea4df072b068bc1c0d96e
DiogoNaN <diogonan@protonmail.ch>
2021-02-22 20:24:42 -0300

    add example routes

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,23 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is running"))
+app.get('/', (req, res) => {
+  return res.json({
+    message: "Hello NLW4",
+  });
+});
+
+app.post('/', (req, res) => {
+  const { name, age } = req.body;
+  return res.json({
+    message: `Hello ${name}`,
+    data: {
+      name,
+      age,
+    }
+  });
+});
+
+app.listen(3333, () => {
+  console.log("Server is running");
+});
